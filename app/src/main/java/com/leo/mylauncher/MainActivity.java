9681bca3be63328a2ac3063a3b0e4d7b2f13dbb3
Leo <leo610306011@gmail.com>
2018-05-18 12:10:35 +0800

    print

--- app/src/main/java/com/leo/mylauncher/MainActivity.java
+++ app/src/main/java/com/leo/mylauncher/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         app.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.print("test");
+                System.out.print("button");
             }
         });
 
